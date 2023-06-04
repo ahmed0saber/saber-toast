10f0651774f56c94e2ed7fd4464261303d05524b
krypton225 <k.mohamed.11.98@gmail.com>
2023-06-04 21:23:44 +0300

    ADD ToastContainer module

---
+++ src/js/ToastContainer.js
@@ -0,0 +1,38 @@
+const SaberToastContainer = (function () {
+    function createBottomRight() {
+        const bottomRightContainer = document.createElement('div');
+        bottomRightContainer.classList.add('saber-toasts-container-bottom-right');
+        bottomRightContainer.classList.add('saber-toasts-containers');
+        document.body.append(bottomRightContainer);
+    }
+
+    function createBottomLeft() {
+        const bottomLeftContainer = document.createElement('div');
+        bottomLeftContainer.classList.add('saber-toasts-container-bottom-left');
+        bottomLeftContainer.classList.add('saber-toasts-containers');
+        document.body.append(bottomLeftContainer);
+    }
+
+    function createTopRight() {
+        const topRightContainer = document.createElement('div');
+        topRightContainer.classList.add('saber-toasts-container-top-right');
+        topRightContainer.classList.add('saber-toasts-containers');
+        document.body.append(topRightContainer);
+    }
+
+    function createTopLeft() {
+        const topLeftContainer = document.createElement('div');
+        topLeftContainer.classList.add('saber-toasts-container-top-left');
+        topLeftContainer.classList.add('saber-toasts-containers');
+        document.body.append(topLeftContainer);
+    }
+
+    return {
+        createBottomLeft,
+        createBottomRight,
+        createTopLeft,
+        createTopRight
+    };
+})();
+
+export default SaberToastContainer;
