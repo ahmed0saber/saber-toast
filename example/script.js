344a86bdc93c33985a7a076c2d3ce178cf54f4a9
krypton225 <k.mohamed.11.98@gmail.com>
2023-06-04 21:32:23 +0300

    UPD using of Toast and its functionalities

--- example/script.js
+++ example/script.js
@@ -1,43 +1,45 @@
+import SaberToast from "../src/js/saber-toast.js";
+
 document.querySelector(".success-btn").addEventListener('click', () => {
-    saberToast.success({
+    new SaberToast().success({
         title: "تمت العملية بنجاح",
         text: "تم اضافة المنتج الى السلة",
         delay: 200,
         duration: 2600,
         rtl: true,
         position: "top-left"
-    })
-})
+    });
+});
 
 document.querySelector(".error-btn").addEventListener('click', () => {
-    saberToast.error({
+    new SaberToast().error({
         title: "حدث خطأ ما",
         text: "يرجى المحاولة مرة اخرى",
         delay: 200,
         duration: 2600,
         rtl: true,
         position: "bottom-right"
-    })
-})
+    });
+});
 
 document.querySelector(".warn-btn").addEventListener('click', () => {
-    saberToast.warn({
+    new SaberToast().warn({
         title: "حدث خطأ ما",
         text: "يرجى المحاولة مرة اخرى",
         delay: 200,
         duration: 2600,
         rtl: true,
         position: "bottom-right"
-    })
-})
+    });
+});
 
 document.querySelector(".info-btn").addEventListener('click', () => {
-    saberToast.info({
+    new SaberToast().info({
         title: "حدث خطأ ما",
         text: "يرجى المحاولة مرة اخرى",
         delay: 200,
         duration: 2600,
         rtl: true,
         position: "bottom-right"
-    })
-})
+    });
+});
