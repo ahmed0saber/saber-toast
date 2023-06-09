a6433983c789dc9be90fceee0f8259cccd8dec0b
Ahmed Saber <ahmed0saber33@gmail.com>
2023-06-09 19:50:39 +0300

    Revert "Update core styles with SCSS & JS of library with modularity (#4)"
    This reverts commit 75ec54af2d4d7379d453d5a0b6114207576910f5.

--- example/script.js
+++ example/script.js
@@ -1,45 +1,43 @@
-import SaberToast from "../src/js/SaberToast.js";
-
-document.querySelector(".success-btn").addEventListener("click", () => {
-    new SaberToast().success({
+document.querySelector(".success-btn").addEventListener('click', () => {
+    saberToast.success({
         title: "تمت العملية بنجاح",
         text: "تم اضافة المنتج الى السلة",
         delay: 200,
         duration: 2600,
         rtl: true,
         position: "top-left"
-    });
-});
+    })
+})
 
-document.querySelector(".error-btn").addEventListener("click", () => {
-    new SaberToast().error({
+document.querySelector(".error-btn").addEventListener('click', () => {
+    saberToast.error({
         title: "حدث خطأ ما",
         text: "يرجى المحاولة مرة اخرى",
         delay: 200,
         duration: 2600,
         rtl: true,
         position: "bottom-right"
-    });
-});
+    })
+})
 
-document.querySelector(".warn-btn").addEventListener("click", () => {
-    new SaberToast().warn({
+document.querySelector(".warn-btn").addEventListener('click', () => {
+    saberToast.warn({
         title: "حدث خطأ ما",
         text: "يرجى المحاولة مرة اخرى",
         delay: 200,
         duration: 2600,
         rtl: true,
         position: "bottom-right"
-    });
-});
+    })
+})
 
-document.querySelector(".info-btn").addEventListener("click", () => {
-    new SaberToast().info({
+document.querySelector(".info-btn").addEventListener('click', () => {
+    saberToast.info({
         title: "حدث خطأ ما",
         text: "يرجى المحاولة مرة اخرى",
         delay: 200,
         duration: 2600,
         rtl: true,
         position: "bottom-right"
-    });
-});
+    })
+})
